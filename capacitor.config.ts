d6aa024e83fbf34887ecc71a1e2b7025dffbb714
Donna6609 <Donnalockett@ymail.com>
2026-03-31 20:36:43 -0400

    Add capacitor.config.ts with correct bundle ID

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.base698ab9ae938dc61b1a405bd7.app',
+  appName: 'FloraSonics',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
